a46df9b7ce627c349178cb6f6f7276394880be2e
TUBEOF <admin@yourgamespace.com>
2021-06-10 16:11:13 +0200

    Updated Config Version

--- src/main/java/com/yourgamespace/anticooldown/data/Data.java
+++ src/main/java/com/yourgamespace/anticooldown/data/Data.java
@@ -5,7 +5,7 @@ public class Data {
 
     public Data() {}
 
-    private final Integer currentConfigVersion = 5;
+    private final Integer currentConfigVersion = 7;
 
     public int getCurrentConfigVersion() {
         return currentConfigVersion;
