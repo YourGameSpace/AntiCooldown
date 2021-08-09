65f4887121d6922e8f552945ad49f4182e107135
TUBEOF <admin@yourgamespace.com>
2021-08-09 03:56:04 +0200

    Updated Plugin Version

--- src/main/java/com/yourgamespace/anticooldown/data/Data.java
+++ src/main/java/com/yourgamespace/anticooldown/data/Data.java
@@ -5,7 +5,7 @@ public class Data {
 
     public Data() {}
 
-    private final Integer currentConfigVersion = 7;
+    private final Integer currentConfigVersion = 8;
 
     public int getCurrentConfigVersion() {
         return currentConfigVersion;
