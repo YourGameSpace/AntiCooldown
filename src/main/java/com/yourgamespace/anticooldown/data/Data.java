afb2b3b5eaef1fb1d1738ca2da6960e734af04c7
TUBEOF <admin@yourgamespace.com>
2021-08-19 23:20:27 +0200

    ProtocolLib NoClassDef Error Fix

--- src/main/java/com/yourgamespace/anticooldown/data/Data.java
+++ src/main/java/com/yourgamespace/anticooldown/data/Data.java
@@ -6,7 +6,7 @@ public class Data {
     public Data() {}
 
     private final Integer currentConfigVersion = 9;
-    private boolean protocollib = false;
+    private boolean protocollib = true;
 
     public int getCurrentConfigVersion() {
         return currentConfigVersion;
