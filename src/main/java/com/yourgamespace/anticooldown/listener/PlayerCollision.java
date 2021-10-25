6f514b15bfeb39f55cb56dc65ecb506f9446a1f6
TUBEOF <46605849+YourGameSpace@users.noreply.github.com>
2021-10-26 01:09:38 +0200

    Added PlayerCollision module

--- src/main/java/com/yourgamespace/anticooldown/listener/PlayerCollision.java
+++ src/main/java/com/yourgamespace/anticooldown/listener/PlayerCollision.java
@@ -3,6 +3,8 @@ package com.yourgamespace.anticooldown.listener;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
+import com.yourgamespace.anticooldown.data.Data;
+import com.yourgamespace.anticooldown.main.AntiCooldown;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,13 +14,18 @@ import java.lang.reflect.InvocationTargetException;
 
 public class PlayerCollision implements Listener {
 
+    private final Data data = AntiCooldown.getData();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+        // Check if ProtocolLib is installed
+        if(!data.isProtocolLibInstalled()) return;
+
         Player player = event.getPlayer();
 
         PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.SCOREBOARD_TEAM);
         packet.getIntegers().write(1, 0); // Mode
-        packet.getStrings().write(0, "testteam"); // Name
+        packet.getStrings().write(0, "disable-collision"); // Name
         packet.getStrings().write(5, "never"); // Collision rule
 
         try {
--- src/main/java/com/yourgamespace/anticooldown/main/AntiCooldown.java
+++ src/main/java/com/yourgamespace/anticooldown/main/AntiCooldown.java
@@ -148,6 +148,7 @@ public class AntiCooldown extends JavaPlugin {
         pluginManager.registerEvents(new ItemRestriction(), this);
         pluginManager.registerEvents(new CustomItemDamage(), this);
         pluginManager.registerEvents(new EnderpearlCooldown(), this);
+        pluginManager.registerEvents(new PlayerCollision(), this);
 
         // Packet Handler
         if(data.isProtocolLibInstalled()) {
