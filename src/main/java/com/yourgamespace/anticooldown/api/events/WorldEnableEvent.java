7759efe282908f4f2ce247298513129448dcc128
TUBEOF <46605849+YourGameSpace@users.noreply.github.com>
2021-11-07 03:46:12 +0100

    Added WorldDisableEvent and WorldEnableEvent for API

---
+++ src/main/java/com/yourgamespace/anticooldown/api/events/WorldEnableEvent.java
@@ -0,0 +1,32 @@
+package com.yourgamespace.anticooldown.api.events;
+
+import org.bukkit.World;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class WorldEnableEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final World world;
+
+    public WorldEnableEvent(World world) {
+        this.world = world;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    /**
+     * Returns the world involved in this event
+     * @return Returns the world object
+     */
+    public World getWorld() {
+        return world;
+    }
+}
