6be8c0644245047c50884c098fad0c9b330474c6
TUBEOF <84939906+TUBEOF@users.noreply.github.com>
2021-11-15 21:00:56 +0100

    Added getter

--- src/main/java/com/yourgamespace/anticooldown/utils/AntiCooldownModule.java
+++ src/main/java/com/yourgamespace/anticooldown/utils/AntiCooldownModule.java
@@ -2,6 +2,7 @@ package com.yourgamespace.anticooldown.utils;
 
 import com.yourgamespace.anticooldown.data.Data;
 import com.yourgamespace.anticooldown.main.AntiCooldown;
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
@@ -38,6 +39,7 @@ public abstract class AntiCooldownModule implements Listener {
      * Check if this module is enabled.
      * @return Returns true, if module is enabled and false if not.
      */
+    @Getter
     public boolean isEnabled() {
         return isEnabled;
     }
@@ -105,6 +107,7 @@ public abstract class AntiCooldownModule implements Listener {
      * Getter for module name.
      * @return Returns the name of the module.
      */
+    @Getter
     public String getModuleName() {
         return moduleName;
     }
@@ -113,6 +116,7 @@ public abstract class AntiCooldownModule implements Listener {
      * Getter for isProtocolLibRequired
      * @return Returns if ProtocolLib is required for the module.
      */
+    @Getter
     public boolean isProtocolLibRequired() {
         return isProtocolLibRequired;
     }
