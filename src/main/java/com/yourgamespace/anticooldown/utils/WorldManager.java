6915b61e69608b903903c31687be3ecfd1a2b04f
TUBEOF <84939906+TUBEOF@users.noreply.github.com>
2021-11-17 20:57:08 +0100

    Fixed NPE with new world manager

--- src/main/java/com/yourgamespace/anticooldown/utils/WorldManager.java
+++ src/main/java/com/yourgamespace/anticooldown/utils/WorldManager.java
@@ -1,6 +1,5 @@
 package com.yourgamespace.anticooldown.utils;
 
-import com.yourgamespace.anticooldown.files.PluginConfig;
 import com.yourgamespace.anticooldown.main.AntiCooldown;
 
 import java.util.ArrayList;
@@ -9,17 +8,16 @@ public class WorldManager {
 
     public WorldManager() {}
 
-    private final PluginConfig pluginConfig = AntiCooldown.getPluginConfig();
     private final ArrayList<String> disabledWorlds = new ArrayList<>();
 
     public void disableWorld(String world) {
         disabledWorlds.add(world);
-        pluginConfig.setDisabledWorld(world, true);
+        AntiCooldown.getPluginConfig().setDisabledWorld(world, true);
     }
 
     public void enableWorld(String world) {
         disabledWorlds.remove(world);
-        pluginConfig.setDisabledWorld(world, false);
+        AntiCooldown.getPluginConfig().setDisabledWorld(world, false);
     }
 
     public void addCache(String world) {
