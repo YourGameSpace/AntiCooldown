96eb71120a3b5898fe704e443e6db235ad5d1e83
TUBEOF <84939906+TUBEOF@users.noreply.github.com>
2021-12-18 05:12:40 +0100

    Use correct constructor for CustomItemDamage

--- module/customitemdamage/src/main/java/com/yourgamespace/anticooldown/module/customitemdamage/main/CustomItemDamage.java
+++ module/customitemdamage/src/main/java/com/yourgamespace/anticooldown/module/customitemdamage/main/CustomItemDamage.java
@@ -3,13 +3,12 @@ package com.yourgamespace.anticooldown.module.customitemdamage.main;
 import com.yourgamespace.anticooldown.module.customitemdamage.files.Config;
 import com.yourgamespace.anticooldown.module.customitemdamage.listeners.ApplyDamage;
 import com.yourgamespace.anticooldown.utils.module.AntiCooldownModule;
-import com.yourgamespace.anticooldown.utils.module.ModuleDescription;
 import org.bukkit.event.Listener;
 
 public class CustomItemDamage extends AntiCooldownModule implements Listener {
 
-    public CustomItemDamage(boolean isProtocolLibRequired, ModuleDescription moduleDescription) {
-        super(isProtocolLibRequired, moduleDescription);
+    public CustomItemDamage(boolean isProtocolLibRequired) {
+        super(false);
     }
 
     private static CustomItemDamage instance;
